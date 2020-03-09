b1cc1031bc111a9260d98883e2c6855f5b3beb23
TuzDmitry <dimatuz94@gmail.com>
2020-03-09 21:36:28 +0300

    2/16 classes

--- src/App.js
+++ src/App.js
@@ -5,13 +5,24 @@ import TodoListTasks from "./TodoListTasks";
 import TodoListFooter from "./TodoListFooter";
 
 class App extends React.Component {
+    tasks=[
+        {title: "JS", isDone: true, priority:'low'},
+        {title: "HTML", isDone: true, priority:'high'},
+        {title: "CSS", isDone: true, priority:'low'},
+        {title: "SaSS", isDone: false, priority:'high'},
+        {title: "React", isDone: false, priority:'low'},
+    ]
+    filterValue ="Completed"
     render = () => {
+
+
+
         return (
             <div className="App">
                 <div className="todoList">
                     <TodoListHeader />
-                    <TodoListTasks />
-                    <TodoListFooter />
+                    <TodoListTasks tasks={this.tasks}/>
+                    <TodoListFooter filterValue={this.filterValue}/>
                 </div>
             </div>
         );
---
+++ src/TodoListTask.jsx
@@ -0,0 +1,18 @@
+import React from "react"
+class TodoListTask extends React.Component {
+    render = () => {
+        return (
+            <div className="">
+                <div className="todoList-tasks">
+                    <div className="todoList-task">
+                        <input type="checkbox" checked={this.props.isDone}/>
+                        <span>{this.props.title}-{this.props.priority}</span>
+                    </div>
+
+                </div>
+            </div>
+        );
+    }
+}
+
+export default TodoListTask;
