271c100d654df659bae81ae05f8abb6d98278045
TuzDmitry <dimatuz94@gmail.com>
2020-03-09 18:08:27 +0300

    Initial commit 1/16

---
+++ src/TodoListFooter.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+class TodoListFooter extends React.Component {
+    render = () => {
+        return (
+            <div className="">
+                <div className="todoList-footer">
+                    <button>All</button>
+                    <button>Completed</button>
+                    <button>Active</button>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default TodoListFooter;
